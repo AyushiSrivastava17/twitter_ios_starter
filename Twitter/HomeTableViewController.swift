3494d2e7f0760afd75b7577b35506ff0bdd63368
Ayushi Srivastava <ayushisri@gmail.com>
2019-05-05 12:22:34 -0700

    Can now favorite/retweet and create tweets

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -19,6 +19,11 @@ class HomeTableViewController: UITableViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweet()
+    }
+    
     func loadTweet(){
         
         let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
@@ -62,6 +67,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileimageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     // MARK: - Table view data source
---
+++ Twitter/tweets.swift
@@ -0,0 +1,10 @@
+//
+//  tweets.swift
+//  Twitter
+//
+//  Created by Ayushi Srivastava on 4/29/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import Foundation
+
